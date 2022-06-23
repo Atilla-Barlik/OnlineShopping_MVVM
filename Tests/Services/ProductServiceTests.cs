9473f6e1482a6517e42ce78e9de88f7d9ed0deef
Gurkanzgl <gurkanozgel58@gmail.com>
2022-06-24 00:37:43 +0300

    ProductServiceTest Olusturuldu.

---
+++ Tests/Services/ProductServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using eShopOnContainers.Core.Services.ProductService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace eShopOnContainers.Test.Services
+{
+    public class ProductServiceTests
+    {
+        [Fact]
+        public async Task getFakeProductItem()
+        {
+            var productMockService = new ProductMockService();
+            var product = await productMockService.GetProduct();
+            Assert.NotEmpty(product);
+        }
+        [Fact]
+        public async Task getFakeCarouselItem()
+        {
+            var carouselMockService = new ProductMockService();
+            var carousel = await carouselMockService.GetCarouselItems();
+            Assert.NotEmpty(carousel);
+        }
+    }
+}
